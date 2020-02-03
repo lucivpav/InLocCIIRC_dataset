d3310b9ab5204603c5145ec8f805fdcafc96db89
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-02-03 15:31:49 +0100

    ability to capture a perspective image from the point cloud

--- projectPointCloud.m
+++ projectPointCloud.m
@@ -1,19 +1,17 @@
 pc = pcread('matterpak_sehs6V3VnSW/cloud - rotated.ply');
 intensities = single(pc.Color(:,1:3))/255;
 points = [pc.Location(:,1:3), intensities];
-f = 10;
-px = 300;
-py = 300;
-K = [f, 0, px; 0, f, py; 0, 0, 1];
-r = [0.17682930550100803, 3.651460591341834, -0.11934799025763472];
-R = eul2rotm(r);
+f = 500;
+rFix = [0.0, 0.0, 180.0];
+r = rFix + [0.17682930550100803, 3.651460591341834, -0.11934799025763472];
 t = [0.0009786105947569013; 1.6932588815689087; 0.06866297125816345];
-P = K * [R, t];
 imageSize = [1000,1000];
 
-[projected, valid] = projectPoints(points, P, [], [], imageSize, false);
-projected = projected(valid,:);
+angles = r*pi/180;
+position = t;
+T = eye(4);
+T(1:3,1:3) = angle2dcm(angles(1),angles(2),angles(3));
+T(1:3,4) = position;
 
-scatter(projected(:,1),projected(:,2),20,projected(:,3:5),'fill');
-axis equal;
-title('Points projected with camera model');
+[ image ] = points2Image(points, imageSize, f, T, 10, 1);
+imshow(image);
