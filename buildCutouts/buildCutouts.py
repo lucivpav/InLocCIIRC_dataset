56381239cd8fd401a0e6f5327afd7bf37fa3740d
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-03-24 22:15:29 +0100

    extract mesh projection into InLocCIIRC_utils

--- buildCutouts/buildCutouts.py
+++ buildCutouts/buildCutouts.py
@@ -9,63 +9,8 @@ import os
 import open3d as o3d
 import py360convert
 
-def buildXYZcut(scene, f, camera_position, rotation_matrix, sensorSize):
-    # In OpenGL, camera points toward -z by default, hence we don't need rFix like in the MATLAB code
-    sensorWidth = sensorSize[0]
-    sensorHeight = sensorSize[1]
-    fovHorizontal = 2*np.arctan((sensorWidth/2)/f)
-    fovVertical = 2*np.arctan((sensorHeight/2)/f)
-
-    camera = pyrender.PerspectiveCamera(fovVertical)
-
-    camera_pose = np.eye(4)
-    camera_pose[0:3,0:3] = rotation_matrix
-    camera_pose[0:3,3] = camera_position
-    scene.add(camera, pose=camera_pose)
-
-    scene._ambient_light = np.ones((3,))
-
-    r = pyrender.OffscreenRenderer(sensorWidth, sensorHeight)
-    #depth = r.render(scene, pyrender.constants.RenderFlags.DEPTH_ONLY)
-    meshProjection, depth = r.render(scene)
-    #plt.figure()
-    #plt.axis('off')
-    #plt.imshow(depth, cmap=plt.cm.gray_r)
-    #plt.show()
-
-    # XYZ cut
-    scaling = 1.0/f
-
-    spaceCoordinateSystem = np.eye(3)
-
-    sensorCoordinateSystem = np.matmul(rotation_matrix, spaceCoordinateSystem)
-    sensorXAxis = sensorCoordinateSystem[:,0]
-    sensorYAxis = -sensorCoordinateSystem[:,1]
-    # make camera point toward -z by default, as in OpenGL
-    cameraDirection = -sensorCoordinateSystem[:,2] # unit vector
-
-    xyzCut = np.zeros((sensorHeight, sensorWidth, 3))
-    #pts = []
-
-    for x in range(-int(sensorWidth/2), int(sensorWidth/2)):
-        for y in range(-int(sensorHeight/2), int(sensorHeight/2)):
-            sensorPoint = camera_position + cameraDirection + \
-                            x * scaling * sensorXAxis + \
-                            y * scaling * sensorYAxis
-            imageX = x + int(sensorWidth/2)
-            imageY = y + int(sensorHeight/2)
-            d = depth[imageY, imageX]
-            sensorPointDir = sensorPoint - camera_position
-            intersectedPoint = camera_position + sensorPointDir * d
-            xyzCut[imageY, imageX, :] = intersectedPoint
-            #pts.append(intersectedPoint)
-    
-    #pts = np.array(pts)
-    #pcd = o3d.geometry.PointCloud()
-    #pcd.points = o3d.utility.Vector3dVector(pts)
-    #o3d.visualization.draw_geometries([pcd])
-
-    return xyzCut, depth, meshProjection
+sys.path.insert(1, os.path.join(sys.path[0], '../functions'))
+from InLocCIIRC_utils.projectMesh.projectMesh import projectMesh
 
 def getSweepRecord(sweepData, panoId):
     for i in range(len(sweepData)):
@@ -94,8 +39,6 @@ if __name__ == '__main__':
     fovVertical = np.rad2deg(fovVertical)
 
     sweepData = sio.loadmat(sweepDataPath, squeeze_me=True)['sweepData']
-    trimeshScene = trimesh.load(meshPath)
-    scene = pyrender.Scene.from_trimesh_scene(trimeshScene)
 
     if not os.path.isdir(cutoutsDir):
         os.mkdir(cutoutsDir)
@@ -133,7 +76,7 @@ if __name__ == '__main__':
             # set up the mat file
             cameraRotation = sweepRecord['rotation'] + np.array([pitch, -yaw, 0.0])
             rotationMatrix = R.from_euler('xyz', cameraRotation, degrees=True).as_matrix()
-            XYZcut, depth, meshProjection = buildXYZcut(scene, f, sweepRecord['position'], rotationMatrix, sensorSize)
+            RGBcut, XYZcut, depth = projectMesh(meshPath, f, rotationMatrix, sweepRecord['position'], sensorSize)
             filename = filename + '.mat'
             path = os.path.join(thisPanoCutoutsDir, filename)
             sio.savemat(path, {'RGBcut': panoramaProjection, 'XYZcut': XYZcut})
@@ -145,4 +88,4 @@ if __name__ == '__main__':
 
                 filename = 'mesh_%d_%d_%d.jpg' % (panoId, yaw, pitch)
                 path = os.path.join(thisPanoCutoutsDir, filename)
-                plt.imsave(path, meshProjection)
+                plt.imsave(path, RGBcut)
