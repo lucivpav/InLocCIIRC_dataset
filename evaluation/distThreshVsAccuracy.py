dbabb2dc0bf12fc37e5ccc6b478e8813ac06a7d5
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-04-11 13:53:11 +0200

    generate distThreshVsAccuracy.csv

---
+++ evaluation/distThreshVsAccuracy.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+def getAccuracy(distanceThreshold, angularThreshold, errorsDf):
+    nCorrect = 0
+    nQueries = len(errorsDf)
+    for i in range(nQueries):
+        translationError = errorsDf['translation'][i]
+        orientationError = errorsDf['orientation'][i]
+        if translationError < distanceThreshold and orientationError < angularThreshold:
+            nCorrect += 1
+    accuracy = nCorrect / nQueries * 100
+    return accuracy
+
+angularThreshold = 10 # in degrees
+datasetDir = '/Volumes/GoogleDrive/Můj disk/ARTwin/InLocCIIRC_dataset'
+evaluationDir = os.path.join(datasetDir, 'evaluation')
+errorsPath = os.path.join(evaluationDir, 'errors.csv')
+outputPath = os.path.join(evaluationDir, 'distThreshVsAccuracy.csv')
+inLocDataPath = 'evaluation/InLocDistThreshVsAccuracy.csv'
+
+errorsDf = pd.read_csv(errorsPath, sep=',')
+inLocDf = pd.read_csv(inLocDataPath, sep=';', header=None)
+outputFile = open(outputPath, 'w')
+
+for i in range(len(inLocDf)):
+    distanceThreshold = inLocDf[0][i]
+    accuracy = getAccuracy(distanceThreshold, angularThreshold, errorsDf) # InLocCIIRC accuracy
+    outputFile.write('%f; %f\n' % (distanceThreshold, accuracy))
+
+outputFile.close()
