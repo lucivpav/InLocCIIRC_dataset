a80d46f8befb34c2c1063a29f8e7b4462cd9fe80
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-04-24 13:25:14 +0200

    find better transformation

--- query/params/holoLens1Params.m
+++ query/params/holoLens1Params.m
@@ -2,7 +2,7 @@ function [ params ] = s10eParams(params)
 
     params.query.dir = fullfile(params.dataset.dir, 'query-HoloLens1');
     %params.camera.rotation.wrt.marker = deg2rad([-10.0 3.0 -5.0]);
-    params.camera.rotation.wrt.marker = deg2rad([-2.0 9.0 -7.0]);
+    params.camera.rotation.wrt.marker = deg2rad([-2.0 7.0 -7.0]);
     params.camera.sensor.size = [756, 1344]; % height, width
     params.camera.fl = 1015; % in pixels
     params.camera.origin.wrt.marker = 0.023 * [3; 20; -3];
