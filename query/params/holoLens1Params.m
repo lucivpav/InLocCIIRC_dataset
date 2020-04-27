dd4e87497951a7c2d37cadd5f62f5ae99448ca82
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-04-27 17:23:45 +0200

    note optimal settings for 3 queries

--- query/params/holoLens1Params.m
+++ query/params/holoLens1Params.m
@@ -1,10 +1,12 @@
 function [ params ] = s10eParams(params)
 
     params.query.dir = fullfile(params.dataset.dir, 'query-HoloLens1');
-    %params.camera.rotation.wrt.marker = deg2rad([-10.0 3.0 -5.0]);
-    params.camera.rotation.wrt.marker = deg2rad([-7.0 6.0 -4.0]);
+    %params.camera.rotation.wrt.marker = deg2rad([-7.0 6.0 -4.0]); % this is optimal for 00132321090555753820.jpg
+    params.camera.rotation.wrt.marker = deg2rad([-2.0 7.0 -7.0]); % this is optimal for 00132321090868821963.jpg
+    %params.camera.rotation.wrt.marker = deg2rad([1.0 9.0 4.0]); % this is optimal for 00132321091341754646.jpg
+    %params.camera.origin.wrt.marker = 0.023 * [5; 20; -3]; % this is optimal for 00132321090555753820.jpg
+    params.camera.origin.wrt.marker = 0.023 * [5; 20; -3]; % this is optimal for 00132321090868821963.jpg
     params.camera.sensor.size = [756, 1344]; % height, width
     params.camera.fl = 1015; % in pixels
-    params.camera.origin.wrt.marker = 0.023 * [5; 20; -3];
     
 end
