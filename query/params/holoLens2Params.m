12f6c1532e6fa3c1c1f15e8e1487a5313fef0f97
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-05-08 22:18:53 +0200

    find better transformation

--- query/params/holoLens2Params.m
+++ query/params/holoLens2Params.m
@@ -9,10 +9,10 @@ function [ params ] = holoLens2Params(params)
     params.HoloLensPoses.dir = fullfile(params.query.dir, 'HoloLensPoses');
     params.HoloLensProjectedPointCloud.dir = fullfile(params.query.dir, 'HoloLensProjectedPointCloud');
     params.HoloLensPosesDelay = 0; % in frames, w.r.t. reference poses
-    params.camera.rotation.wrt.marker = deg2rad([4.0 0.0 5.0]); % this aims to be generic
-    params.camera.origin.wrt.marker = 0.023 * [0; 20; -10];
+    params.camera.rotation.wrt.marker = deg2rad([2.0 2.0 4.0]); % this aims to be generic
+    params.camera.origin.wrt.marker = 0.023 * [0; 13; -5];
     params.camera.sensor.size = [756, 1344]; % height, width
     params.camera.fl = 1015; % in pixels
-    params.HoloLensViconSyncConstant = 29.1 * 1000; % [ms]; prev: <29.0, 29.3>
+    params.HoloLensViconSyncConstant = 28.9 * 1000; % [ms]; expected: <28.6, 29.4>
     
 end
