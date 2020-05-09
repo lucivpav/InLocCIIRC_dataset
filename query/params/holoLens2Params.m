09cd827055ef157c9633831f461d40e4e8fe01d0
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-05-09 16:58:13 +0200

    find decent transformations for same sync constant

--- query/params/holoLens2Params.m
+++ query/params/holoLens2Params.m
@@ -10,13 +10,16 @@ function [ params ] = holoLens2Params(params)
     params.HoloLensPoses.dir = fullfile(params.query.dir, 'HoloLensPoses');
     params.HoloLensProjectedPointCloud.dir = fullfile(params.query.dir, 'HoloLensProjectedPointCloud');
     params.HoloLensPosesDelay = 0; % in frames, w.r.t. reference poses
-    %params.camera.rotation.wrt.marker = deg2rad([2.0 2.0 4.0]); % this is near optimal for query 1
-    %params.camera.origin.wrt.marker = 0.023 * [0; 13; -5]; % this is near optimal for query 1
-    params.camera.rotation.wrt.marker = deg2rad([0.0 2.0 5.0]); % this is near optimal for query 2
-    params.camera.origin.wrt.marker = 0.023 * [4; 16; -10]; % this is near optimal for query 2
+    params.camera.rotation.wrt.marker = deg2rad([2.0 2.0 3.0]); % this is near optimal for query 1
+    %params.camera.rotation.wrt.marker = deg2rad([-1.0 3.0 4.0]); % this is near optimal for query 2
+    %params.camera.rotation.wrt.marker = deg2rad([-1.0 2.0 3.0]); % this is aims to be generic
+    params.camera.origin.wrt.marker = 0.023 * [-3; 13; -5]; % this is near optimal for query 1
+    %params.camera.origin.wrt.marker = 0.023 * [3; 13; -7]; % this is near optimal for query 2
+    %params.camera.origin.wrt.marker = 0.023 * [3; 13; -5]; % this aims to be generic
     params.camera.sensor.size = [756, 1344]; % height, width
     params.camera.fl = 1015; % in pixels
-    %params.HoloLensViconSyncConstant = 28.9 * 1000; % [ms]; expected: <28.8, 29.4> % this is near optimal for query 1
-    params.HoloLensViconSyncConstant = 29.1 * 1000; % [ms]; expected: <28.8, 29.4> % this is near optimal for query 2
+    params.HoloLensViconSyncConstant = 29.0 * 1000; % [ms]; expected: <28.8, 29.4> % this is near optimal for query 1
+    %params.HoloLensViconSyncConstant = 29.0 * 1000; % [ms]; expected: <28.8, 29.4> % this is near optimal for query 2
+    %params.HoloLensViconSyncConstant = 29.0 * 1000; % [ms]; expected: <28.8, 29.4> % this aims to be generic
     
 end
