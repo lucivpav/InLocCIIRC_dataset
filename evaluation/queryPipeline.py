f9cbf50860e47d6ceaf0efe8087c52df7865287c
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-07-16 12:29:26 +0200

    queryPipeline: PE, PV modes; transformPoses: choose projectPointCloud or projectMesh

--- evaluation/queryPipeline.py
+++ evaluation/queryPipeline.py
@@ -16,7 +16,7 @@ def saveFigure(fig, path, width, height):
     img = img.resize((width, height), resample=Image.NEAREST)
     img.save(path)
 
-def renderForQuery(queryId):
+def renderForQuery(queryId, mode):
     inlierColor = '#00ff00'
     inlierMarkerSize = 3
     targetWidth = 600
@@ -24,19 +24,28 @@ def renderForQuery(queryId):
     targetAspectRatio = 1344/756 # HoloLens
     cutoutSize = [1600, 1200] # width, height
     targetHeight = np.round(targetWidth / targetAspectRatio).astype(np.int64)
+    extension = '.png'
 
     datasetDir = '/Volumes/GoogleDrive/Můj disk/ARTwin/InLocCIIRC_dataset'
     #queryDir = os.path.join(datasetDir, 'query-s10e')
     queryDir = os.path.join(datasetDir, 'query-HoloLens1')
     #outputDir = os.path.join(datasetDir, 'outputs-s10e')
-    outputDir = os.path.join(datasetDir, 'outputs')
+    #outputDir = os.path.join(datasetDir, 'outputs')
+    outputDir = os.path.join(datasetDir, 'outputs-HL1-v2')
     cutoutDir = os.path.join(datasetDir, 'cutouts')
-    densePVPath = os.path.join(outputDir, 'densePV_top10_shortlist.mat')
-    #densePVPath = os.path.join(outputDir, 'densePE_top100_shortlist.mat')
+
+    if mode == 'PV':
+        shortlistPath = os.path.join(outputDir, 'densePV_top10_shortlist.mat')
+    elif mode == 'PE':
+        shortlistPath = os.path.join(outputDir, 'densePE_top100_shortlist.mat')
+    else:
+        raise 'Unsupported mode!'
+
     denseInlierDir = os.path.join(outputDir, 'PnP_dense_inlier')
     synthesizedDir = os.path.join(outputDir, 'synthesized')
     #evaluationDir = os.path.join(datasetDir, 'evaluation-s10e-v1')
-    evaluationDir = os.path.join(datasetDir, 'evaluation')
+    #evaluationDir = os.path.join(datasetDir, 'evaluation')
+    evaluationDir = os.path.join(datasetDir, 'evaluation-HL1-v2')
     queryPipelineDir = os.path.join(evaluationDir, 'queryPipeline')
 
     queryName = str(queryId) + '.jpg'
@@ -45,7 +54,7 @@ def renderForQuery(queryId):
     query = matlabEngine.load_query_image_compatible_with_cutouts(queryPath, matlab.double(cutoutSize), nargout=1)
     query = np.asarray(query)
 
-    ImgList = sio.loadmat(densePVPath, squeeze_me=True)['ImgList']
+    ImgList = sio.loadmat(shortlistPath, squeeze_me=True)['ImgList']
     ImgListRecord = next((x for x in ImgList if x['queryname'] == queryName), None)
     cutoutPath = ImgListRecord['topNname'][0]
 
@@ -65,8 +74,12 @@ def renderForQuery(queryId):
 
     if not os.path.isdir(queryPipelineDir):
         os.mkdir(queryPipelineDir)
+    
+    modeDir = os.path.join(queryPipelineDir, mode)
+    if not os.path.isdir(modeDir):
+        os.mkdir(modeDir)
 
-    thisQueryPipelineDir = os.path.join(queryPipelineDir, queryName)
+    thisQueryPipelineDir = os.path.join(modeDir, queryName)
 
     if not os.path.isdir(thisQueryPipelineDir):
         os.mkdir(thisQueryPipelineDir)
@@ -75,29 +88,31 @@ def renderForQuery(queryId):
     plt.imshow(query)
     plt.plot(inls_2d[0,:], inls_2d[1,:], '.', markersize=inlierMarkerSize, color=inlierColor)
     queryNameNoExt = queryName.split('.')[0]
-    queryStepPath = os.path.join(thisQueryPipelineDir, 'query_' + queryNameNoExt + '.png')
+    queryStepPath = os.path.join(thisQueryPipelineDir, 'query_' + queryNameNoExt + extension)
     saveFigure(fig, queryStepPath, targetWidth, targetHeight)
 
     fig = plt.figure()
     plt.imshow(cutout)
     plt.plot(inls_2d[2,:], inls_2d[3,:], '.', markersize=inlierMarkerSize, color=inlierColor)
-    cutoutStepPath = os.path.join(thisQueryPipelineDir, 'chosen_' + buildCutoutName(cutoutPath, '.png'))
+    cutoutStepPath = os.path.join(thisQueryPipelineDir, 'chosen_' + buildCutoutName(cutoutPath, extension))
     saveFigure(fig, cutoutStepPath, targetWidth, targetHeight)
 
-    synthStepPath = os.path.join(thisQueryPipelineDir, 'synthesized.png')
+    synthStepPath = os.path.join(thisQueryPipelineDir, 'synthesized' + 'PV.' + extension)
     synth = Image.fromarray(synth)
     synth = synth.resize((targetWidth, targetHeight), resample=Image.NEAREST)
     synth = np.asarray(synth)
     plt.imsave(synthStepPath, synth)
 
     # NOTE: the errmap typically does not have the same aspect ratio, so it will be stretched
-    errmapStepPath = os.path.join(thisQueryPipelineDir, 'errmap.png')
+    errmapStepPath = os.path.join(thisQueryPipelineDir, 'errmap' + extension)
     errmap = Image.fromarray(errmap)
     errmap = errmap.resize((targetWidth, targetHeight), resample=Image.NEAREST)
     errmap = np.asarray(errmap)
     plt.imsave(errmapStepPath, errmap, cmap='jet')
 
 matlabEngine.addpath(r'functions/InLocCIIRC_utils/at_netvlad_function',nargout=0)
-queryIds = [127]
-for queryId in queryIds:
-    renderForQuery(queryId)
+modes = ['PE', 'PV']
+queryIds = [1,127,200]
+for mode in modes:
+    for queryId in queryIds:
+        renderForQuery(queryId, mode)
--- query/transformPoses.m
+++ query/transformPoses.m
@@ -4,9 +4,12 @@ addpath('../functions/InLocCIIRC_utils/P_to_str');
 addpath('../functions/InLocCIIRC_utils/params');
 addpath('../functions/InLocCIIRC_utils/R_to_numpy_array');
 addpath('../functions/InLocCIIRC_utils/projectPointCloud');
+addpath('../functions/InLocCIIRC_utils/projectMesh');
+addpath('../functions/InLocCIIRC_utils/environment');
 [ params ] = setupParams('holoLens1');
 
 justEvaluateOnMatches = false; % TODO: this currently throws when used with holoLens1Params
+useProjectMeshInsteadOfProjectPC = false;
 
 rawPosesTable = readtable(params.rawPoses.path);
 
@@ -18,7 +21,7 @@ if justEvaluateOnMatches
     return;
 end
 
-mkdirIfNonExistent(params.projectedPointCloud.dir);
+mkdirIfNonExistent(params.projectedPoses.dir);
 mkdirIfNonExistent(params.poses.dir);
 
 createDescriptionsFile = true;
@@ -67,24 +70,35 @@ for i=1:size(rawPosesTable,1)
     fprintf(poseFile, '%s', P_str);
     fclose(poseFile);
 
-    pointSize = 8.0;
-    f = params.camera.fl; % in pixels
-    sensorSize = params.camera.sensor.size; % height, width
-    outputSize = sensorSize;
-    projectedPointCloud = projectPointCloud(params.pointCloud.path, f, R, ...
-                                        t, sensorSize, outputSize, pointSize, ...
-                                        params.projectPointCloudPy.path);
+    if useProjectMeshInsteadOfProjectPC
+        f = params.camera.fl;
+        sensorSize = params.camera.sensor.size; % height, width
+        sensorSize = [sensorSize(2), sensorSize(1)]; % width, height
+        headless = ~strcmp(environment(), 'laptop');
+        rFix = [180.0, 0.0, 0.0];
+        Rfix = rotationMatrix(deg2rad(rFix), 'XYZ');
+        actualR = inv(R)*Rfix; % TODO: why is this black magic needed?
+        [projectedPose, ~, ~] = projectMesh(params.mesh.path, f, actualR, t, sensorSize, false, -1, params.input.projectMesh_py_path, headless);
+    else
+        pointSize = 8.0;
+        f = params.camera.fl; % in pixels
+        sensorSize = params.camera.sensor.size; % height, width
+        outputSize = sensorSize;
+        projectedPose = projectPointCloud(params.pointCloud.path, f, R, ...
+                                            t, sensorSize, outputSize, pointSize, ...
+                                            params.projectPointCloudPy.path);
+    end
 
-    imshow(projectedPointCloud);
+    imshow(projectedPose);
     queryFilename = sprintf('%d.jpg', id);
-    outPCFilename = sprintf('%d-PC.jpg', id);
+    outProjectedFilename = sprintf('%d-projected.jpg', id);
     outQueryFilename = sprintf('%d-query.jpg', id);
 
-    outPCPath = fullfile(params.projectedPointCloud.dir, outPCFilename);
-    imwrite(projectedPointCloud, outPCPath);
+    outProjectedPath = fullfile(params.projectedPoses.dir, outProjectedFilename);
+    imwrite(projectedPose, outProjectedPath);
 
     queryImg = imread(fullfile(params.dataset.query.dir, queryFilename));
-    outQueryPath = fullfile(params.projectedPointCloud.dir, outQueryFilename);
+    outQueryPath = fullfile(params.projectedPoses.dir, outQueryFilename);
     imwrite(queryImg, outQueryPath);
     
     [inMap, closestCutout] = isQueryInMap(P, params.spaceName, cutoutDescriptions, params);
